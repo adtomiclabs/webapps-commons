1df5eda2b7cd64ffc58262b2351b873d34f312e9
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-02-11 18:41:48 -0300

    Add commons dtos artifact
    - Just include api_errors for now

---
+++ commons-dtos/src/main/java/com/parabrisassi/sist/commons/dtos/api_errors/NotPresentReferenceErrorDto.java
@@ -0,0 +1,20 @@
+package com.parabrisassi.sist.commons.dtos.api_errors;
+
+import com.parabrisassi.sist.commons.errors.NotPresentReferenceError;
+
+import java.util.List;
+
+/**
+ * Data transfer object for client errors caused by referencing an entity that does not exist.
+ */
+public final class NotPresentReferenceErrorDto extends EntityErrorDto<NotPresentReferenceError> {
+
+    /**
+     * Constructor.
+     *
+     * @param errors The {@link List} of {@link NotPresentReferenceError}s.
+     */
+    public NotPresentReferenceErrorDto(List<NotPresentReferenceError> errors) {
+        super(ErrorFamily.NOT_PRESENT_REFERENCE, errors);
+    }
+}
---
+++ commons-dtos/src/main/java/com/parabrisassi/sist/commons/dtos/api_errors/UniqueViolationErrorDto.java
@@ -0,0 +1,18 @@
+package com.parabrisassi.sist.commons.dtos.api_errors;
+
+import com.parabrisassi.sist.commons.errors.UniqueViolationError;
+
+import java.util.List;
+
+/**
+ * Data transfer object for client errors caused by trying to set a value that is already used and must be unique.
+ */
+public final class UniqueViolationErrorDto extends EntityErrorDto<UniqueViolationError> {
+
+    /**
+     * @param errors The {@link List} of {@link UniqueViolationError}s.
+     */
+    public UniqueViolationErrorDto(List<UniqueViolationError> errors) {
+        super(ErrorFamily.UNIQUE_VIOLATION, errors);
+    }
+}
