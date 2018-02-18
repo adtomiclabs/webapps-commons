9457a8a06d657c29b7021cdc54f84f4db5cf2ca0
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-02-18 05:56:49 -0300

    Add commons authentication artifact
    - It implements some of the authentication interfaces

---
+++ commons-authentication/src/main/java/com/parabrisassi/sist/commons/authentication/EnableJwtAuthentication.java
@@ -0,0 +1,18 @@
+package com.parabrisassi.sist.commons.authentication;
+
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.*;
+
+/**
+ * Enables the jwt authentication system, by importing needed beans into the scope.
+ * In order to be used, an {@link AuthenticationTokenBlacklistedChecker} bean must be defined,
+ * and the "com.parabrisassi.sist.authentication.jwt.key.public" property must be set.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@Inherited
+@Documented
+@Import(JwtAuthenticationConfigurer.class)
+public @interface EnableJwtAuthentication {
+}
