98c0a2046229fc5e8534dc72039d409690d6ede0
Juan Marcos Bellini <juanmbellini@gmail.com>
2018-02-19 03:30:10 -0300

    Fix JerseyController javadoc
    - It had an empty <p>, which make the package process log a warning message

--- commons-web-config/src/main/java/com/parabrisassi/sist/commons/config/JerseyController.java
+++ commons-web-config/src/main/java/com/parabrisassi/sist/commons/config/JerseyController.java
@@ -9,7 +9,7 @@ import java.lang.annotation.*;
 /**
  * Indicates that a class is a Jersey Controller.
  * <p>
- * <p>This annotation serves as a specialization of {@link Component},
+ * This annotation serves as a specialization of {@link Component},
  * allowing for implementation classes to be autodetected through classpath scanning,
  * and {@link Provider}, allowing annotated classes to be detected for registering by package registration by Jersey.
  */
